247929c2408776e87a88ec38f2b1633ee642f2b8
Josh Varty <joshvarty@gmail.com>
2016-09-27 23:54:14 -0700

    Added another solution to consider.

--- DailyProjectEuler/2016.09.27.Problem2/2016.09.27.Problem2.cpp
+++ DailyProjectEuler/2016.09.27.Problem2/2016.09.27.Problem2.cpp
@@ -32,9 +32,29 @@ int mySolution()
 	return sum;
 }
 
+int anotherSolution()
+{
+	int x = 0;
+	int y = 1;
+	int z;
+	int sum = 0;
+
+	do {
+		z = x + y;
+		x = y;
+		y = z;
+		if (y % 2 == 0) sum += y;
+	} while (y <= 4000000);
+
+	return sum;
+}
+
 int main()
 {
-	int result = mySolution();
+	int result = anotherSolution();
+	std::cout << result << "\n";
+
+	result = anotherSolution();
 	std::cout << result << "\n";
 
 	//Don't close the window
