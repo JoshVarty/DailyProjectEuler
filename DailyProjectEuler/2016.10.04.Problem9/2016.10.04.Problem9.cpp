02ffebf714dcb47330288bfb16e2c898d3bb2d0c
Josh Varty <joshvarty@gmail.com>
2016-10-12 21:56:05 -0700

    Added my solution to Problem 9

--- DailyProjectEuler/2016.10.04.Problem9/2016.10.04.Problem9.cpp
+++ DailyProjectEuler/2016.10.04.Problem9/2016.10.04.Problem9.cpp
@@ -6,7 +6,7 @@
 
 //	A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 //
-//	a2 + b2 = c2
+//	a^2 + b^2 = c^2
 //	For example, 32 + 42 = 9 + 16 = 25 = 52.
 //
 //	There exists exactly one Pythagorean triplet for which a + b + c = 1000.
@@ -15,7 +15,25 @@
 
 int getAnswer()
 {
+	for (int a = 1; a < 999; a++)
+	{
+		int aSquared = a * a;
+		for (int b = 1; b < 999; b++)
+		{
+			int bSquared = b * b;
+			int aSquaredPlusBSquared = aSquared + bSquared;
+			for (int c = 1; c < 999; c++)
+			{
+				int cSquared = c * c;
+				if (aSquaredPlusBSquared == cSquared && a + b + c == 1000)
+				{
+					return a * b * c;
+				}
+			}
+		}
+	}
 
+	return -1;
 }
 
 int main()
