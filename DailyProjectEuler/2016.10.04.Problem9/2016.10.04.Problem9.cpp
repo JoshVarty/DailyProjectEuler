f73d20f8524659e2741db00dab606a4eb0826fad
Josh Varty <joshvarty@gmail.com>
2016-10-12 20:27:55 -0700

    Setup for Project 9

---
+++ DailyProjectEuler/2016.10.04.Problem9/2016.10.04.Problem9.cpp
@@ -0,0 +1,32 @@
+// 2016.10.04.Problem9.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+//	A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+//
+//	a2 + b2 = c2
+//	For example, 32 + 42 = 9 + 16 = 25 = 52.
+//
+//	There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+//	Find the product abc.
+
+
+int getAnswer()
+{
+
+}
+
+int main()
+{
+	long long int result = getAnswer();
+
+	std::cout << result << "\n";
+
+	//Don't close the window
+	std::cin >> result;;
+
+    return 0;
+}
+
