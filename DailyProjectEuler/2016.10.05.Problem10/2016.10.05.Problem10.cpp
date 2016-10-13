e73b0744499f5dd153b42b2800016445cc1fa3e7
Josh Varty <joshvarty@gmail.com>
2016-10-12 22:18:03 -0700

    Setup for Problem10

---
+++ DailyProjectEuler/2016.10.05.Problem10/2016.10.05.Problem10.cpp
@@ -0,0 +1,14 @@
+// 2016.10.05.Problem10.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+//The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+//
+//Find the sum of all the primes below two million.
+
+int main()
+{
+    return 0;
+}
+
