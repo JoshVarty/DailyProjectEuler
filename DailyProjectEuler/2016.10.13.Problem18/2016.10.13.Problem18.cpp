032ee6984c793792dc2f0c24f18c02ac697d1297
Josh Varty <joshvarty@gmail.com>
2016-10-18 21:42:04 -0700

    Setup for Problem18

---
+++ DailyProjectEuler/2016.10.13.Problem18/2016.10.13.Problem18.cpp
@@ -0,0 +1,22 @@
+// 2016.10.13.Problem18.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+long long getAnswer()
+{
+    return 0;
+}
+
+int main()
+{
+    long long result = getAnswer();
+
+    std::cout << result << "\n";
+
+    //Wait for input 
+    std::cin >> result;
+
+    return 0;
+}
