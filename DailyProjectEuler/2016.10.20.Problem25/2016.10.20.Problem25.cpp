bdefe97f15a53a8c40af22e27f4788066e876475
Josh Varty <joshvarty@gmail.com>
2016-10-24 23:16:19 -0700

    Setup for Problem 25

---
+++ DailyProjectEuler/2016.10.20.Problem25/2016.10.20.Problem25.cpp
@@ -0,0 +1,24 @@
+// 2016.10.20.Problem25.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+long long getAnswer()
+{
+	return 0;
+}
+
+
+int main()
+{
+	long long result = getAnswer();
+
+	std::cout << result << "\n";
+
+	std::cin >> result;
+
+	return 0;
+}
+
