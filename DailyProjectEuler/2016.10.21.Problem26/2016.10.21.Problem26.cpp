ca74951954c0eca05f8969365a931c8adfe6a090
Josh Varty <joshvarty@gmail.com>
2016-11-01 01:27:20 -0700

    Added my solution.
    I'm not a fan of this question.

--- DailyProjectEuler/2016.10.21.Problem26/2016.10.21.Problem26.cpp
+++ DailyProjectEuler/2016.10.21.Problem26/2016.10.21.Problem26.cpp
@@ -4,11 +4,58 @@
 #include "stdafx.h"
 #include <iostream>
 
+#define MAX_SIZE 1000 
+
+
+int getCycleLength(int divisor)
+{
+	int * firstSeen = new int[MAX_SIZE]();
+
+	for (int i = 0; i < MAX_SIZE; i++)
+	{
+		firstSeen[i] = -1;
+	}
+
+	int remainder = 1;
+	int index = 0;
+	while (remainder != 0 && firstSeen[remainder] == -1)
+	{
+		firstSeen[remainder] = index;
+
+		remainder = remainder * 10;
+		remainder = remainder % divisor;
+
+		index = index + 1;
+	}
+
+	int cycleLength = index - firstSeen[remainder];
+	free(firstSeen);
+	
+	if (remainder == 0)
+	{
+		return 0;
+	}
+
+	return cycleLength;
+}
+
 
 long long getAnswer()
 {
+	int maxCycleLength = 0;
+	int numWithMaxCycle = -1;
+	
+	for (int i = 2; i < 1000; i++)
+	{
+		int cycleLength = getCycleLength(i);
+		if (cycleLength > maxCycleLength)
+		{
+			maxCycleLength = cycleLength;
+			numWithMaxCycle = i;
+		}
+	}
 
-	return 0;
+	return numWithMaxCycle;
 }
 
 int main()
