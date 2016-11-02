8701dd7e9da0c0f7dbb63275b45d7fb1e32c3cd5
Josh Varty <joshvarty@gmail.com>
2016-11-01 22:30:40 -0700

    Add solution for Problem 27

--- DailyProjectEuler/2016.10.22.Problem27/2016.10.22.Problem27.cpp
+++ DailyProjectEuler/2016.10.22.Problem27/2016.10.22.Problem27.cpp
@@ -4,9 +4,88 @@
 #include "stdafx.h"
 #include <iostream>
 
+#define AMAX 1000
+#define BMAX 1000
+
+int * knownPrimes;
+
+bool isPrime(long long number)
+{
+	number = abs(number);
+	if (number == 0) return false;
+	if (number == 1)
+	{
+		return false;
+	}
+	if (number == 2)
+	{
+		return true;
+	}
+
+	if (knownPrimes[number] == 1)
+	{
+		return true;
+	}
+	else if (knownPrimes[number] == -1)
+	{
+		return false;
+	}
+
+	long long int root = sqrt(number);
+
+	for (long long int i = 2; i <= root; i++)
+	{
+		if (number % i == 0)
+		{
+			knownPrimes[number] = -1;
+			return false;
+		}
+	}
+
+	knownPrimes[number] = 1;
+	return true;
+}
+
+int getNumberOfPrimes(int a, int b)
+{
+	int primesSoFar = 0;
+	int n = 0;
+	bool numberIsPrime = true;
+
+	do
+	{
+		n = n + 1;
+		long long result = n * n + a * n + b;
+		bool numberIsPrime = isPrime(result);
+
+	} while (numberIsPrime);
+
+	return n;
+}
+
 long long getAnswer()
 {
-	return 0;
+	knownPrimes = new int[INT32_MAX]();
+
+	int currentMax = 0;
+	int maxA = 0;
+	int maxB = 0;
+
+	for (int a = -1000; a < AMAX; a++)
+	{
+		for (int b = -1000; b < BMAX; b++)
+		{
+			int numberOfPrimes = getNumberOfPrimes(a, b);
+			if (numberOfPrimes > currentMax)
+			{
+				currentMax = numberOfPrimes;
+				maxA = a;
+				maxB = b;
+			}
+		}
+	}
+
+	return maxA * maxB;
 }
 
 int main()
